bf3a408960c8473fc76816c2ae5e7407aeeb4b48
andrij19941 <106135455+andrij19941@users.noreply.github.com>
2023-07-16 21:11:33 +0300

    444

--- dist/js/scripts.min.js
+++ dist/js/scripts.min.js
@@ -1,8 +1,8 @@
 new LazyLoad();
 
-var link = document.createElement('link');
-link.rel = 'stylesheet';
-link.href = 'base-stylesheet.sass';
+// var link = document.createElement('link');
+// link.rel = 'stylesheet';
+// link.href = 'base-stylesheet.sass';
 
-// Вставка елементу <link> на сторінку
-document.head.appendChild(link);
+// // Вставка елементу <link> на сторінку
+// document.head.appendChild(link);
